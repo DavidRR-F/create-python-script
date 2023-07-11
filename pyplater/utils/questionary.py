22b66f44f550f7b8c9bc041e2ab2820c0ff77a8f
DavidRR-F <david.rosefranklin96@gmail.com>
2023-07-10 20:05:28 -0400

    ci config init

--- pyplater/utils/questionary.py
+++ pyplater/utils/questionary.py
@@ -1,6 +1,7 @@
 import click
 import questionary
 
+
 class QuestionaryInput(click.Option):
     def __init__(self, param_decls=None, **attrs):
         click.Option.__init__(self, param_decls, **attrs)
@@ -8,22 +9,25 @@ class QuestionaryInput(click.Option):
     def prompt_for_value(self, ctx):
         val = questionary.text(self.prompt).ask()
         return val
-    
+
+
 class QuestionaryConfirm(click.Option):
     def __init__(self, param_decls=None, **attrs):
         click.Option.__init__(self, param_decls, **attrs)
 
     def prompt_for_value(self, ctx):
         val = questionary.confirm(self.prompt).ask()
-        return val    
-    
-class QuestionaryOption(click.Option):
+        return val
 
+
+class QuestionaryOption(click.Option):
     def __init__(self, param_decls=None, **attrs):
         click.Option.__init__(self, param_decls, **attrs)
         if not isinstance(self.type, click.Choice):
-            raise Exception('Choice Option type arg must be click.Choice')
+            raise Exception("Choice Option type arg must be click.Choice")
 
     def prompt_for_value(self, ctx):
-        val = questionary.select(f"Choose {self.prompt}:", choices=self.type.choices).unsafe_ask()
+        val = questionary.select(
+            f"Choose {self.prompt}:", choices=self.type.choices
+        ).unsafe_ask()
         return val
