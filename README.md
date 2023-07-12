86d14667f2736cc55836ee95cfc9ab43ba6b9e24
DavidRR-F <david.rosefranklin96@gmail.com>
2023-07-11 22:30:33 -0400

    unit test/ci init

--- README.md
+++ README.md
@@ -4,22 +4,20 @@
 
 **_in development_**
 
-<!-- for reference when published -->
-<!-- <p align="center">
-<a href="https://github.com/tiangolo/fastapi/actions?query=workflow%3ATest+event%3Apush+branch%3Amaster" target="_blank">
-    <img src="https://github.com/tiangolo/fastapi/workflows/Test/badge.svg?event=push&branch=master" alt="Test">
-</a>
-<a href="https://coverage-badge.samuelcolvin.workers.dev/redirect/tiangolo/fastapi" target="_blank">
-    <img src="https://coverage-badge.samuelcolvin.workers.dev/tiangolo/fastapi.svg" alt="Coverage">
-</a>
-<a href="https://pypi.org/project/fastapi" target="_blank">
-    <img src="https://img.shields.io/pypi/v/fastapi?color=%2334D058&label=pypi%20package" alt="Package version">
-</a>
-<a href="https://pypi.org/project/fastapi" target="_blank">
-    <img src="https://img.shields.io/pypi/pyversions/fastapi.svg?color=%2334D058" alt="Supported Python versions">
-</a>
-</p> -->
-
+<div align="center">
+  <a href="link-to-your-repository">
+    <img src="https://img.shields.io/badge/Python-v3.8.1%2B-brightgreen" alt="Python Version">
+  </a>
+  <a href="link-to-your-repository">
+    <img src="https://img.shields.io/badge/Version-${{ steps.version.outputs.version }}-blue" alt="Package Version">
+  </a>
+  <a href="link-to-your-workflow">
+    <img src="https://img.shields.io/github/workflow/status/DavidRR-F/pyplater/test?label=Test%20Status" alt="Test Status">
+  </a>
+  <a href="link-to-coverage-report">
+    <img src="https://img.shields.io/badge/Coverage-${{ env.CODE_COVERAGE }}%25-brightgreen" alt="Coverage Status">
+  </a>
+</div>
 PyPlater is a Python CLI Tool to generate, build, and create boilerplate code for python projects Including linting, formatting, unit testing, and package managing from prebuilt and your own custom templates
 
 # Get Setup
--- pyplater/tests/test_cli.py
+++ pyplater/tests/test_cli.py
@@ -1,13 +1,177 @@
+from click.testing import CliRunner
+from unittest import mock
+from pyplater.cli import pyplater
+import shutil
 import pytest
+import os
 
 
 @pytest.fixture
-def setup_and_teardown():
-    print("Setting up")
-    yield "Hello, test!"
-    print("Tearing down")
+def setup_and_teardown(tmp_path, request):
+    yield
+    temp_dir = getattr(request.node, "temp_dir", None)
+    # Teardown: Delete the directory
+    if temp_dir is not None and os.path.exists(temp_dir):
+        shutil.rmtree(temp_dir)
 
 
-def test_example(setup_and_teardown):
-    print(setup_and_teardown)
-    print("Running test")
+def test_save_template(tmp_path, request, setup_and_teardown):
+    # Prepare test directory and file
+    source_dir = tmp_path / "src"
+    source_dir.mkdir()
+    with open(source_dir / "test.txt", "w") as f:
+        f.write("test_project content")
+
+    runner = CliRunner()
+    result = runner.invoke(
+        pyplater, ["save", str(source_dir), "test_project", "--type", "Template"]
+    )
+
+    # Check if the command ran successfully
+    assert result.exit_code == 0
+    assert result.output == "test_project has been saved as a Template\n"
+
+    # Send Teardown Path
+    temp_dir = os.path.join(
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+        "templates/test_project",
+    )
+    request.node.temp_dir = temp_dir
+
+
+def test_save_snippet(tmp_path, request, setup_and_teardown):
+    # Prepare test directory and file
+    source_dir = tmp_path / "src"
+    source_dir.mkdir()
+    with open(source_dir / "test.txt", "w") as f:
+        f.write("test_project content")
+
+    runner = CliRunner()
+    result = runner.invoke(
+        pyplater, ["save", str(source_dir), "test_project", "--type", "Snippet"]
+    )
+
+    # Check if the command ran successfully
+    assert result.exit_code == 0
+    assert result.output == "test_project has been saved as a Snippet\n"
+
+    # Send Teardown Path
+    temp_dir = os.path.join(
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+        "snippets/test_project",
+    )
+    request.node.temp_dir = temp_dir
+
+
+def test_remove_template(setup_and_teardown):
+    # Prepare test data: Create a template directory
+    templates_dir = os.path.join(
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+        "templates/test_template",
+    )
+
+    os.mkdir(templates_dir)
+
+    # Run the command
+    runner = CliRunner()
+    with mock.patch("questionary.confirm") as mock_confirm:
+        mock_confirm.return_value.ask.return_value = True
+        result = runner.invoke(
+            pyplater,
+            ["remove", "test_template", "--type", "Template"],
+            catch_exceptions=False,
+        )
+
+    # Check if the command ran successfully
+    assert result.exit_code == 0
+    assert result.output == "Test_Template deleted\n"
+    assert not os.path.exists(templates_dir)
+
+
+def test_remove_snippet(setup_and_teardown):
+    # Prepare test data: Create a template directory
+    snippet_dir = os.path.join(
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+        "snippets/test_snippet",
+    )
+
+    os.mkdir(snippet_dir)
+
+    # Run the command
+    runner = CliRunner()
+    with mock.patch("questionary.confirm") as mock_confirm:
+        mock_confirm.return_value.ask.return_value = True
+        result = runner.invoke(
+            pyplater,
+            ["remove", "test_snippet", "--type", "Snippet"],
+            catch_exceptions=False,
+        )
+
+    # Check if the command ran successfully
+    assert result.exit_code == 0
+    assert result.output == "Test_Snippet deleted\n"
+    assert not os.path.exists(snippet_dir)
+
+
+def test_add_snippet(tmp_path, setup_and_teardown):
+    os.chdir(tmp_path)
+    # Set up the input for the command
+    snippet_name = "sqlalchemy"
+    # Run the command
+    runner = CliRunner()
+    result = runner.invoke(
+        pyplater,
+        ["add", "--snippet", snippet_name],
+    )
+
+    # Check if the command ran successfully
+    assert result.exit_code == 0
+
+    # Perform additional assertions as needed
+
+    # Example: Check if the supporting files were added
+    assert (tmp_path / "db").exists()
+
+
+def test_create_project(tmp_path, setup_and_teardown):
+    os.chdir(tmp_path)
+    # Set up the input for the command
+    template_name = "starter"
+    # Run the command
+    runner = CliRunner()
+    result = runner.invoke(
+        pyplater,
+        ["create", "--name", "test_project", "--template", template_name],
+    )
+
+    # Check if the command ran successfully
+    assert result.exit_code == 0
+
+    # Perform additional assertions as needed
+
+    # Example: Check if the supporting files were added
+    assert (tmp_path / "test_project").exists()
+
+
+def test_run_script(tmp_path):
+    os.chdir(tmp_path)
+    # Prepare test data
+    script_name = "script"
+
+    # Mock the pyproject.toml content
+    pyproject_content = """
+        [pyplater.scripts]
+        script = 'python my_script.py'
+    """
+    with open("my_script.py", "w") as f:
+        f.write(pyproject_content)
+
+    with open("pyproject.toml", "w") as f:
+        f.write(pyproject_content)
+
+    # Run the command
+    runner = CliRunner()
+    result = runner.invoke(pyplater, ["run", script_name])
+
+    # Check if the command ran successfully
+    assert result.exit_code == 0
